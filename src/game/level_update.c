caa6a3c513bcf05a4468b315f011d42a39e7cbfb
Gregory Heskett <gheskett@gmail.com>
2025-01-03 00:41:34 -0500

    But like, for real this time

--- src/game/level_update.c
+++ src/game/level_update.c
@@ -1216,12 +1216,7 @@ s32 init_level(void) {
                 set_mario_action(gMarioState, ACT_IDLE, 0);
             } else if (!gDebugLevelSelect) {
                 if (gMarioState->action != ACT_UNINITIALIZED) {
-                    if (save_file_exists(gCurrSaveFileNum - 1)) {
-                        set_mario_action(gMarioState, ACT_IDLE, 0);
-                    } else {
-                        set_mario_action(gMarioState, ACT_INTRO_CUTSCENE, 0);
-                        val4 = TRUE;
-                    }
+                    set_mario_action(gMarioState, ACT_IDLE, 0);
                 }
             }
 #else
