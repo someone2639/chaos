0f4223d1ffdc8f6832de1c6f0a4d8b875cf67a36
someone2639 <someone2639@gmail.com>
2025-02-03 19:23:43 -0500

    Ortho patch

--- src/game/chaos/chaos_patchdata.c
+++ src/game/chaos/chaos_patchdata.c
@@ -1646,4 +1646,16 @@ const struct ChaosPatch gChaosPatches[CHAOS_PATCH_COUNT] = {
         .name              = "Rainbow Stars",
         .shortDescription  = "Shoutouts to SimpleFlips.",
     },
+    [CHAOS_PATCH_ORTHO] = {
+        .durationType      = CHAOS_DURATION_STARS,
+        .effectType        = CHAOS_EFFECT_NEGATIVE,
+        .severity          = 1,
+        .isStackable       = FALSE,
+        .duration          = 5,
+
+        .conditionalFunc   = chs_cond_ortho,
+
+        .name              = "Orthographic Mode",
+        .shortDescription  = "A whole new perspective! Or really a lack of one...",
+    },
 };
