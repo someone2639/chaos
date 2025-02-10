f9e1795589c6e555dc61152f94f0c22ec0d360b5
Anoliah <173389381+Anoliah@users.noreply.github.com>
2025-02-10 06:51:24 -0800

    fix noclip death plane in jrb

--- src/game/level_update.c
+++ src/game/level_update.c
@@ -876,7 +876,7 @@ s16 level_trigger_warp(struct MarioState *m, s32 warpOp) {
 
             case WARP_OP_WARP_FLOOR:
                 sSourceWarpNodeId = WARP_NODE_WARP_FLOOR;
-                if (area_get_warp_node(sSourceWarpNodeId) == NULL) {
+                if (area_get_warp_node(sSourceWarpNodeId) == NULL || (gCurrLevelNum == LEVEL_JRB && m->floor->type == SURFACE_DEATH_PLANE)) { // epic hard coded level check
                     if (gChaosLivesEnabled && m->numLives <= 0 && !chaos_check_if_patch_active(CHAOS_PATCH_MIRACLE)) {
                         sDelayedWarpOp = WARP_OP_GAME_OVER;
                     } else {
