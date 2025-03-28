ac8bbd961194a5cd54ebd7f5088fe2212fc368ea
Anoliah <173389381+Anoliah@users.noreply.github.com>
2025-03-28 15:24:44 -0700

    add transparent key model to use when key is already obtained

--- levels/scripts.c
+++ levels/scripts.c
@@ -110,6 +110,7 @@ const LevelScript level_main_scripts_entry[] = {
     LOAD_MODEL_FROM_GEO(MODEL_MARIOS_CAP,              marios_cap_geo), // repeated
     LOAD_MODEL_FROM_GEO(MODEL_BOWSER_KEY_CUTSCENE,     bowser_key_cutscene_geo),
     LOAD_MODEL_FROM_GEO(MODEL_BOWSER_KEY,              bowser_key_geo),
+    LOAD_MODEL_FROM_GEO(MODEL_TRANSPARENT_BOWSER_KEY,  transparent_bowser_key_geo),
     LOAD_MODEL_FROM_GEO(MODEL_RED_FLAME_SHADOW,        red_flame_shadow_geo),
     LOAD_MODEL_FROM_GEO(MODEL_1UP,                     mushroom_1up_geo),
     LOAD_MODEL_FROM_GEO(MODEL_GREEN_DEMON,             green_demon_geo),
