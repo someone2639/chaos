accad7f3d457740cb9fa0ee8c8c59c676dcfc466
Anoliah <173389381+Anoliah@users.noreply.github.com>
2025-04-25 11:20:54 -0700

    make kaizo block coin consistent with other coin behavior

--- src/game/behaviors/kaizo_block.inc.c
+++ src/game/behaviors/kaizo_block.inc.c
@@ -1,6 +1,8 @@
 #define KBLOCK_ACT_1_FRAMES 3
 #define KBLOCK_ACT_2_FRAMES 3
 
+u32 interact_coin(struct MarioState *, u32, struct Object *);
+
 void bhv_kaizo_block_loop(void) {
     switch(o->oAction) {
         case 0:
@@ -8,10 +10,11 @@ void bhv_kaizo_block_loop(void) {
             o->oPosY += (10 * sins(((0xFFFF / 4) / KBLOCK_ACT_1_FRAMES) * o->oTimer));
             if(o->oTimer > KBLOCK_ACT_1_FRAMES) {
                 o->oAction++;
-                gMarioState->numCoins++;
                 o->oAnimState++;
-                spawn_object_abs_with_rot(o, 0, MODEL_YELLOW_COIN, bhvKaizoBlockCoin,
+                struct Object *coinObj = spawn_object_abs_with_rot(o, 0, MODEL_YELLOW_COIN, bhvKaizoBlockCoin,
                             o->oPosX, o->oPosY + 50, o->oPosZ, 0, 0, 0);
+                coinObj->oDamageOrCoinValue = 1;
+                interact_coin(gMarioState, INTERACT_COIN, coinObj);
             }
             break;
         case 1:
