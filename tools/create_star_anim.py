bc677eb399c7d129848eafbfea70e29515fa4e4a
someone2639 <someone2639@gmail.com>
2026-09-09 23:22:42 -0400

    smooth animations anyway

--- tools/create_star_anim.py
+++ tools/create_star_anim.py
@@ -1,8 +1,8 @@
-startpos = [1200, 1200, 0]
+startpos = [-150, -800, 0]
 endpos = [-900, 800, 0] # silver star
 # endpos = [-150, 800, 0] # red star
 
-byframe = 30
+byframe = 45
 
 NUM_FRAMES = 164
 
@@ -11,12 +11,16 @@ intermediate_positions = [startpos]
 def approach_f32_asymptotic(current: float, target: float, multiplier: float):
     if (multiplier > 1.0): multiplier = 1.0
 
-    return (current + ((target - current) * multiplier))
+    return current + ((target - current) * multiplier)
 
-MULTIPLIER = 0.5
+MULTIPLIER = 0.2
 
 for i in range(byframe):
-    intermediate_positions.append(intermediate_positions[-1])
+    intermediate_positions.append([
+        approach_f32_asymptotic(intermediate_positions[-1][0], endpos[0], MULTIPLIER),
+        approach_f32_asymptotic(intermediate_positions[-1][1], endpos[1], MULTIPLIER),
+        approach_f32_asymptotic(intermediate_positions[-1][2], endpos[2], MULTIPLIER)
+    ])
 
 for i in range(NUM_FRAMES - byframe):
     intermediate_positions.append(endpos)
